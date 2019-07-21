79348780a8aa2b50e796652881cbe0a00c478ab7
MaironCss <mironk96@mail.ru>
2019-07-21 10:16:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+;(function($){
+	"use strict";
+
+	$('.ba-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: false,
+		infinity: true,
+		autoplay: true,
+		autoplaySpeed: 1000;
+	});
+})(jQuery);
+
